47494c423cf35f364c1b7e36180fd36856349e51
Michael Hart <michael.hart.au@gmail.com>
2016-07-10 12:39:03 -0400

    Add Rust build

--- build-rust.sh
+++ build-rust.sh
@@ -16,10 +16,23 @@ export RUSTUP_HOME=$HOME/.multirust/rustup
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
+# Remove Rust docs to save space
+rm -rf $RUSTUP_HOME/toolchains/stable-x86_64-unknown-linux-gnu/share
+
 export PATH=$HOME/.cargo/bin:$PATH
+
+# Begin the GCC bootstrapping process
+
+curl -sSL https://lambci.s3.amazonaws.com/binaries/gcc-4.8.5.tgz | tar -C /tmp -xz
+
+export PATH=/tmp/bin:/tmp/sbin:$PATH
+export LD_LIBRARY_PATH=/usr/local/lib64/node-v4.3.x/lib:/tmp/lib:/tmp/lib64:/lib64:/usr/lib64:/var/runtime:/var/task:/var/task/lib
+export CPATH=/tmp/include
+export LIBRARY_PATH=/tmp/lib
 
 
 # Now we can run our project's install and test steps
 
-cargo build --verbose
-cargo test --verbose
+cargo build --verbose --color=always
+cargo test --verbose --color=always
+
