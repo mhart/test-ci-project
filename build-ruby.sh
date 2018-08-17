e11e92cf7d346045b37ba5337ebbb7cb8fbcf86a
Michael Hart <michael.hart.au@gmail.com>
2018-08-17 16:29:41 -0400

    Try nats-io/pure-ruby-nats

--- build-ruby.sh
+++ build-ruby.sh
@@ -3,9 +3,9 @@
 # Clone an example Ruby repo
 # This step obviously won't be necessary if you're building a Ruby repo with LambCI,
 # because you'll already be in the cloned repo directory
-rm -rf /tmp/http-2
-git clone --depth 1 https://github.com/igrigorik/http-2 /tmp/http-2
-cd /tmp/http-2
+rm -rf /tmp/pure-ruby-nats
+git clone --depth 1 https://github.com/nats-io/pure-ruby-nats /tmp/pure-ruby-nats
+cd /tmp/pure-ruby-nats
 
 
 # You could just put this in your `cmd` config property
