1fca76572e570e5f7722922a93ba0c0e5efbc9b4
Michael Hart <michael.hart.au@gmail.com>
2018-08-17 16:27:22 -0400

    Exec rake

--- build-ruby.sh
+++ build-ruby.sh
@@ -9,4 +9,4 @@ cd /tmp/http-2
 
 
 # You could just put this in your `cmd` config property
-. ~/init/ruby && bundle install && bundle exec thor spec
+. ~/init/ruby && bundle install && bundle exec rake
