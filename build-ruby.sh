7b17de38c64916fd7b5c96a91fab3a4c754c50d8
Michael Hart <michael.hart.au@gmail.com>
2018-08-17 16:57:04 -0400

    Try halostatue/diff-lcs

--- build-ruby.sh
+++ build-ruby.sh
@@ -3,9 +3,9 @@
 # Clone an example Ruby repo
 # This step obviously won't be necessary if you're building a Ruby repo with LambCI,
 # because you'll already be in the cloned repo directory
-rm -rf /tmp/pure-ruby-nats
-git clone --depth 1 https://github.com/nats-io/pure-ruby-nats /tmp/pure-ruby-nats
-cd /tmp/pure-ruby-nats
+rm -rf /tmp/diff-lcs
+git clone --depth 1 https://github.com/halostatue/diff-lcs /tmp/diff-lcs
+cd /tmp/diff-lcs
 
 
 # You could just put this in your `cmd` config property
