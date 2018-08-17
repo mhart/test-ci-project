4ea5e38e5e7fb790c53f89ee527e0fb9c25c0a67
Michael Hart <michael.hart.au@gmail.com>
2018-08-17 16:26:49 -0400

    Try igrigorik/http-2 for the Ruby project

--- build-ruby.sh
+++ build-ruby.sh
@@ -3,9 +3,9 @@
 # Clone an example Ruby repo
 # This step obviously won't be necessary if you're building a Ruby repo with LambCI,
 # because you'll already be in the cloned repo directory
-rm -rf /tmp/thor
-git clone --depth 1 https://github.com/erikhuda/thor /tmp/thor
-cd /tmp/thor
+rm -rf /tmp/http-2
+git clone --depth 1 https://github.com/igrigorik/http-2 /tmp/http-2
+cd /tmp/http-2
 
 
 # You could just put this in your `cmd` config property
