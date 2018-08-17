99c602d87fa0c15028bae6d212306bfc4eb378d5
Michael Hart <michael.hart.au@gmail.com>
2018-08-17 17:51:58 -0400

    Try cakephp/chronos for PHP build

--- build-php.sh
+++ build-php.sh
@@ -3,9 +3,9 @@
 # Clone an example PHP repo
 # This step obviously won't be necessary if you're building a PHP repo with LambCI,
 # because you'll already be in the cloned repo directory
-rm -rf /tmp/laravel
-git clone --depth 1 https://github.com/laravel/framework /tmp/laravel
-cd /tmp/laravel
+rm -rf /tmp/chronos
+git clone --depth 1 https://github.com/cakephp/chronos /tmp/chronos
+cd /tmp/chronos
 
 
 # You could just put this in your `cmd` config property
