82977bf6b3f379e6d13e7b0541070c8c15a0f0ce
Michael Hart <michael.hart.au@gmail.com>
2018-08-17 16:25:06 -0400

    Try erikhuda/thor for the Ruby project

--- build-ruby.sh
+++ build-ruby.sh
@@ -3,10 +3,10 @@
 # Clone an example Ruby repo
 # This step obviously won't be necessary if you're building a Ruby repo with LambCI,
 # because you'll already be in the cloned repo directory
-rm -rf /tmp/json
-git clone --depth 1 https://github.com/flori/json /tmp/json
-cd /tmp/json
+rm -rf /tmp/thor
+git clone --depth 1 https://github.com/erikhuda/thor /tmp/thor
+cd /tmp/thor
 
 
 # You could just put this in your `cmd` config property
-. ~/init/ruby && bundle install && bundle exec rake
+. ~/init/ruby && bundle install && bundle exec thor spec
