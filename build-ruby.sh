b9f7f765a42b280d196c2a52013253da5a340fa8
Michael Hart <michael.hart.au@gmail.com>
2018-08-20 11:33:27 -0400

    Fix diff-lcs at v1.3

--- build-ruby.sh
+++ build-ruby.sh
@@ -4,7 +4,7 @@
 # This step obviously won't be necessary if you're building a Ruby repo with LambCI,
 # because you'll already be in the cloned repo directory
 rm -rf /tmp/diff-lcs
-git clone --depth 1 https://github.com/halostatue/diff-lcs /tmp/diff-lcs
+git clone -b v1.3 --depth 1 https://github.com/halostatue/diff-lcs /tmp/diff-lcs
 cd /tmp/diff-lcs
 
 
