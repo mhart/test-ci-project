1e746c935d8402be35341b91057b9f05c64e7311
Michael Hart <michael.hart.au@gmail.com>
2018-08-20 11:34:58 -0400

    Fix pytest to 3.7.2

--- build-python.sh
+++ build-python.sh
@@ -4,7 +4,7 @@
 # This step obviously won't be necessary if you're building a Python repo with LambCI,
 # because you'll already be in the cloned repo directory
 rm -rf /tmp/pytest
-git clone --depth 50 https://github.com/pytest-dev/pytest /tmp/pytest
+git clone -b 3.7.2 --depth 1 https://github.com/pytest-dev/pytest /tmp/pytest
 cd /tmp/pytest
 
 
