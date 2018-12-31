3eaec219e720bd88761f4a6685a177790e224a24
Michael Hart <michael.hart.au@gmail.com>
2018-12-31 17:18:19 -0500

    Update PHP build to chronos 1.2.3

--- build-php.sh
+++ build-php.sh
@@ -4,8 +4,8 @@
 # This step obviously won't be necessary if you're building a PHP repo with LambCI,
 # because you'll already be in the cloned repo directory
 rm -rf /tmp/chronos
-git clone -b 1.2.2 --depth 1 https://github.com/cakephp/chronos /tmp/chronos
+git clone -b 1.2.3 --depth 1 https://github.com/cakephp/chronos /tmp/chronos
 cd /tmp/chronos
 
 # You could just put this in your `cmd` config property
-. ~/init/php && composer install -n --prefer-dist && vendor/bin/phpunit
+. ~/init/php 7.2.12 && composer install -n --prefer-dist && vendor/bin/phpunit
