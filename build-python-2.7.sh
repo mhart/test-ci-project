259260428eb665f1d3b251d14332a94db0e2673a
Michael Hart <michael.hart.au@gmail.com>
2019-12-10 12:48:09 -0500

    Try pytest 4.6.7 on Python 2.7

--- build-python-2.7.sh
+++ build-python-2.7.sh
@@ -4,9 +4,9 @@
 # This step obviously won't be necessary if you're building a Python repo with LambCI,
 # because you'll already be in the cloned repo directory
 rm -rf /tmp/pytest
-git clone -b 3.7.2 --depth 1 https://github.com/pytest-dev/pytest /tmp/pytest
+git clone -b 4.6.7 --depth 1 https://github.com/pytest-dev/pytest /tmp/pytest
 cd /tmp/pytest
 
 
 # You could just put this in your `cmd` config property
-pip install --user tox && tox --recreate -e py27
+pip install --user tox && TOXENV=py27 tox
