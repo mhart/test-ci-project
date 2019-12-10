0652ee82531b955d789ca83ef8cb9e9e3022db9d
Michael Hart <michael.hart.au@gmail.com>
2019-12-10 16:01:25 -0500

    Try Ruby 2.4.9

---
+++ build-ruby-2.4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+
+# Clone an example Ruby repo
+# This step obviously won't be necessary if you're building a Ruby repo with LambCI,
+# because you'll already be in the cloned repo directory
+rm -rf /tmp/diff-lcs
+git clone -b v1.3 --depth 1 https://github.com/halostatue/diff-lcs /tmp/diff-lcs
+cd /tmp/diff-lcs
+
+
+# You could just put this in your `cmd` config property
+. ~/init/ruby 2.4.9 && bundle install && bundle exec rake
