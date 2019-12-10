775beb8df64f72c504223bfaf5bc35fe81f54100
Michael Hart <michael.hart.au@gmail.com>
2019-12-10 16:14:25 -0500

    Try Ruby 2.6 again with gcc

--- build-ruby-2.6.sh
+++ build-ruby-2.6.sh
@@ -7,6 +7,8 @@ rm -rf /tmp/diff-lcs
 git clone -b v1.3 --depth 1 https://github.com/halostatue/diff-lcs /tmp/diff-lcs
 cd /tmp/diff-lcs
 
+# Need this because of the native json extension
+. ~/init/gcc
 
 # You could just put this in your `cmd` config property
 . ~/init/ruby 2.6.5 && bundle install && bundle exec rake
