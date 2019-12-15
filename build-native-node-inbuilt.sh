6156ce278ad472a077da26607ce72c0141bf229f
Michael Hart <michael.hart.au@gmail.com>
2019-12-15 17:45:07 -0500

    Try native inbuilt gcc

---
+++ build-native-node-inbuilt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+# Clone an example native Node.js repo
+# This step obviously won't be necessary if you're building a native Node.js repo with LambCI,
+# because you'll already be in the cloned repo directory
+rm -rf /tmp/node.bcrypt.js
+git clone --depth 1 https://github.com/kelektiv/node.bcrypt.js -b v3.0.7 /tmp/node.bcrypt.js
+cd /tmp/node.bcrypt.js
+
+
+# No need to install gcc, it should be a layer on the function
+npm test
+
